d48bdb09890e00eaae5e3d585dad55ef7afb2ad3
Chris <christopherpack17@gmail.com>
2025-02-04 22:36:16 -0600

    Fixed some design and added form

--- data/reference.js
+++ data/reference.js
@@ -1,64 +1,64 @@
 export const studentList = [
   {
     id: 1,
-    name: "",
-    house: "",
+    name: "Link",
+    title: "Warrior",
     phoenix: true,
     death: false,
     img: "../assets/images/g.png"
   },
   {
     id: 2,
-    name: "",
-    house: "",
+    name: "Goliath",
+    title: "Elder",
     phoenix: true,
     death: false,
     img: "../assets/images/g.png"
   },
   {
     id: 3,
-    name: "",
-    house: "",
+    name: "Hermes",
+    title: "Scholar",
     phoenix: true,
     death: false,
     img: "../assets/images/r.png"
   },
   {
     id: 4,
-    name: "",
-    house: "",
+    name: "Aurthur",
+    title: "King",
     phoenix: true,
     death: false,
     img: "../assets/images/g.png"
   },
   {
     id: 5,
-    name: "",
-    house: "",
+    name: "Merlin",
+    title: "Wizard",
     phoenix: true,
     death: false,
     img: "../assets/images/s.png"
   },
   {
     id: 6,
-    name: "",
-    house: "",
+    name: "Gilgamesh",
+    title: "Warrior",
     phoenix: true,
     death: false,
     img: "../assets/images/g.png"
   },
   {
     id: 7,
-    name: "",
-    house: "",
+    name: "Bilbo",
+    title: "Thief",
     phoenix: true,
     death: false,
     img:"../assets/images/h.png"
   },
   {
     id: 8,
-    name: "",
-    house: "",
+    name: "Eowin",
+    title: "Shield Maiden",
     phoenix: true,
     death: false,
     img: "../assets/images/g.png"
